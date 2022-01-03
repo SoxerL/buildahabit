9682b6ddc4d5d46015039bdf16d26d696f9682f6
SoxerL <sacha.leemann@yahoo.de>
2022-01-03 20:25:34 +0100

    spring initializer

---
+++ src/main/java/net/ddns/buildahabit/BuildahabitApplication.java
@@ -0,0 +1,13 @@
+package net.ddns.buildahabit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildahabitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildahabitApplication.class, args);
+	}
+
+}
